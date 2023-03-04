051e1a5a42ea5e6523f3185a30586fa30f203258
Bruno da Silva Cunha <bruno.cunha2@external.stellantis.com>
2023-03-04 17:13:51 -0300

    [M1S07] Ex 3 - Criar Footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { Navbar } from '../components/Navbar.jsx'
+import { Footer } from '../components/Footer.jsx'
 
 export const Home = () => {
     return (
@@ -6,6 +7,7 @@ export const Home = () => {
             <Navbar />
             <h1>StackEd</h1>
             <div>Your school for technology and web development!</div>
+            <Footer />
         </>
     )
 }
